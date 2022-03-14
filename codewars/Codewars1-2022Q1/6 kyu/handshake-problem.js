64e0e48988530115524a00d217afc936d44cb4d3
vladimirovicp <vladimirovicp70@gmail.com>
2022-03-14 17:02:16 +0400

    add handshake-problem

--- codewars/Codewars1-2022Q1/6 kyu/handshake-problem.js
+++ codewars/Codewars1-2022Q1/6 kyu/handshake-problem.js
@@ -1,9 +1,14 @@
-function getParticipants(handshakes){
+function getParticipants(handshakes) {
     // ...
+    let people = 1;
+    for (let i = 0; i < handshakes; i = i + people++) {
+        console.log(i + ' | ' + people);
+    };
+    return people;
 }
 
 
 //Чужое решение
 
 
-document.getElementById("result").textContent = getParticipants(6);
+document.getElementById("result").textContent = getParticipants(3);
