e9337c3678295b86c5a9459adfa8f479fa23be0f
vladimirovicp <vladimirovicp70@gmail.com>
2022-03-18 12:08:41 +0400

    codewar 6 of 23

--- codewars/Codewars1-2022Q1/6 kyu/duplicate-encoder.js
+++ codewars/Codewars1-2022Q1/6 kyu/duplicate-encoder.js
@@ -5,10 +5,64 @@
 
 function duplicateEncode(word){
     // ...
+
+    let rezult = '';
+    let text ='';
+
+    for (let i=0; i<word.length; i++){
+
+        text = word.slice(0, i) + word.slice(i + 1);
+
+        if(text.includes(word[i].toLowerCase()) || text.includes(word[i].toUpperCase())){
+
+            rezult += ')'
+        } else {
+            rezult += '('
+        }
+    }
+    return rezult;
 }
 
 
 //Чужое решение
 
 
-document.getElementById("result").textContent = duplicateEncode("din");
+// function duplicateEncode(word){
+//     return word
+//         .toLowerCase()
+//         .split('')
+//         .map( function (a, i, w) {
+//             return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+//         })
+//         .join('');
+// }
+
+// function duplicateEncode(word){
+//
+//     var unique='';
+//     word = word.toLowerCase();
+//     for(var i=0; i<word.length; i++){
+//         if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){
+//             unique += '(';
+//         }
+//         else{
+//             unique += ')';
+//         }
+//     }
+//     return unique;
+//
+// }
+
+// function duplicateEncode(word) {
+//     word = word.toLowerCase();
+//     return word.replace(/./g, m => word.indexOf(m) == word.lastIndexOf(m) ? '(' : ')');
+// }
+
+// function duplicateEncode(word) {
+//     var letters = word.toLowerCase().split('')
+//     return letters.map(function(c, i) {
+//         return letters.some(function(x, j) { return x === c && i !== j }) ? ')' : '('
+//     }).join('')
+// }
+
+document.getElementById("result").textContent = duplicateEncode("Success");
