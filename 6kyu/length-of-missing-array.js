3c85fb1de94e2a58c232768c5e2ac31da45758d8
vladimirovicp <vladimirovicp70@gmail.com>
2022-03-20 16:14:18 +0400

    Запуск задачь с помощью node js

---
+++ 6kyu/length-of-missing-array.js
@@ -0,0 +1,11 @@
+function getLengthOfMissingArray(arrayOfArrays) {
+    return 0;
+  }
+
+//getLengthOfMissingArray([ [ 1, 2 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]]);
+
+//let toApp = getLengthOfMissingArray([ [ 1, 2 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]]);
+
+let toApp = 'dsgdfgdf'
+export default toApp;
+
---
+++ app.js
@@ -0,0 +1,6 @@
+import  toApp from './6kyu/length-of-missing-array.js';
+
+
+// console.log('text app');
+
+console.log(toApp);
